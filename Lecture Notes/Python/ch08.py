05d776b8ca6d67d6d9b7893f4ed761dbadd81d7d
Kenji Sato <kenji@kenjisato.jp>
2020-08-01 07:59:04 +0900

    update

--- Lecture Notes/Python/ch08.py
+++ Lecture Notes/Python/ch08.py
@@ -57,8 +57,8 @@ ss
 
 #---- ch08/many-symbols
 T = 80
-c = sp.symbols(f"c[0:{T+2}]")
-k = sp.symbols(f"k[0:{T+1}]")
+c = sp.symbols(f"c[:{T+2}]")
+k = sp.symbols(f"k[:{T+1}]")
 
 c[:3], c[-3:]
 
@@ -90,7 +90,7 @@ c_sol = np.r_[solution[T:], ss[1]]
 
 
 #---- ch08/show/plot
-plt.quiver(k_sol[1:], c_sol[1:], 
+plt.quiver(k_sol[:-1], c_sol[:-1], 
            k_sol[1:] - k_sol[:-1], 
            c_sol[1:] - c_sol[:-1],
            scale_units='xy', angles='xy', scale=1)
@@ -100,17 +100,30 @@ plt.ylabel(r"$c_{t+1}$")
 plt.show()
 
 
-#---- ch08/function
+#---- ch08/solve_ss
+def solve_ss(eqm_k, eqm_c, param, x0):
+    c, k = sp.symbols("c, k")
+    eqm = sp.Matrix([eqm_k.subs({k0: k, k1: k, c0: c, c1: c}),
+                     eqm_c.subs({k0: k, k1: k, c0: c, c1: c})])
+    
+    eqm_lam = sp.lambdify([[k, c]], np.squeeze(eqm.subs(param)))
+    J_lam = sp.lambdify([[k, c]], eqm.jacobian([k, c]).subs(param))
+    
+    ss = fsolve(func=eqm_lam, x0=x0, fprime=J_lam)
+    return ss
+
+
+#---- ch08/solve_optimal_growth
 def solve_optimal_growth(eqm_k, eqm_c, k_init, c_final, param, T):
     
-    c = sp.symbols(f"c[0:{T+2}]")
-    k = sp.symbols(f"k[0:{T+1}]")
-    eqm = sp.Matrix([[EK.subs({k0: k[i], k1: k[i+1], c0: c[i+1], c1: c[i+2]}),
-                  EC.subs({k0: k[i], k1: k[i+1], c0: c[i+1], c1: c[i+2]})] 
-                  for i in range(T)])
+    c = sp.symbols(f"c[:{T+2}]")
+    k = sp.symbols(f"k[:{T+1}]")
+    eqm = sp.Matrix([[eqm_k.subs({k0: k[i], k1: k[i+1], c0: c[i+1], c1: c[i+2]}),
+                      eqm_c.subs({k0: k[i], k1: k[i+1], c0: c[i+1], c1: c[i+2]})] 
+                      for i in range(T)])
     eqm_col = eqm.T.reshape(2*T, 1)
     
-    eqm_param = eqm_col.subs(params).subs({k[0]: k_init, c[-1]: ss[1]})
+    eqm_param = eqm_col.subs(param).subs({k[0]: k_init, c[-1]: c_final})
     eqm_param = np.squeeze(eqm_param)
     
     eqm_num = sp.lambdify([[*k[1:], *c[1:-1]]], eqm_param)
@@ -123,12 +136,35 @@ def solve_optimal_growth(eqm_k, eqm_c, k_init, c_final, param, T):
 
 
 #---- ch08/example/plot
-k_sol, c_sol = solve_optimal_growth(EK, EC, 30.0, ss[1], params, 60)
+ss1 = solve_ss(EK, EC, params, x0=[4, 4])
+k_sol, c_sol = solve_optimal_growth(EK, EC, 30.0, ss1[1], params, 50)
 
-plt.quiver(k_sol[1:], c_sol[1:], 
-          k_sol[1:] - k_sol[:-1], 
-          c_sol[1:] - c_sol[:-1],
-          scale_units='xy', angles='xy', scale=1)
+plt.quiver(k_sol[:-1], c_sol[:-1], 
+           k_sol[1:] - k_sol[:-1], 
+           c_sol[1:] - c_sol[:-1],
+           scale_units='xy', angles='xy', scale=1)
+
+plt.xlabel(r"$k_{t}$")
+plt.ylabel(r"$c_{t+1}$")
+plt.show()
+
+
+#---- ch08/new-ss
+params.update({g: 0.03})
+ss2 = solve_ss(EK, EC, params, x0=[4, 4])
+
+
+#---- ch08/transition
+sol2 = solve_optimal_growth(EK, EC, ss1[0], ss2[1], params, 80)
+k_sol2 = np.r_[ss1[0], sol2[0]] 
+c_sol2 = np.r_[ss1[1], sol2[1]]
+
+
+#---- ch08/transition-plot/plot
+plt.quiver(k_sol2[:-1], c_sol2[:-1], 
+           k_sol2[1:] - k_sol2[:-1], 
+           c_sol2[1:] - c_sol2[:-1],
+           scale_units='xy', angles='xy', scale=1)
 
 plt.xlabel(r"$k_{t}$")
 plt.ylabel(r"$c_{t+1}$")
